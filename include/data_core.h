13ac089d70be8c7d48bf201f6d8c7ab47d3564f9
miou-zora <alexandre.franquet@epitech.eu>
2023-04-10 17:57:49 +0200

    rm(struct): remove useless struct

--- include/data_core.h
+++ include/data_core.h
@@ -13,12 +13,3 @@
 #define MAX_NAME_LENGTH 32
 #define MAX_DESCRIPTION_LENGTH 255
 #define MAX_BODY_LENGTH 512
-
-typedef struct team_entry_s {
-    char *name[MAX_NAME_LENGTH];
-    char *description[MAX_DESCRIPTION_LENGTH];
-
-    LIST_ENTRY(team_entry_s) next_team;
-} team_entry_t;
-
-LIST_HEAD(team_list, team_entry_s);
