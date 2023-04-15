e994516ec575f9c280a1441418924d89091d6eb8
miou-zora <alexandre.franquet@epitech.eu>
2023-04-15 13:15:16 +0200

    refacto(client): move declaration of variable in select_cycle

--- src/client/src/select_cycle.c
+++ src/client/src/select_cycle.c
@@ -11,11 +11,12 @@
 int select_cycle(int client_fd)
 {
     fd_set readfds;
+    int max_fd = client_fd + 1;
+    int activity = select(max_fd, &readfds, NULL, NULL, NULL);
+
     FD_ZERO(&readfds);
     FD_SET(client_fd, &readfds);
     FD_SET(0, &readfds);
-    int max_fd = client_fd + 1;
-    int activity = select(max_fd, &readfds, NULL, NULL, NULL);
     if (activity < 0)
         return (84);
     if (FD_ISSET(client_fd, &readfds))
