ce25e181bc77544307a0a892322e6f29a434fd46
miou-zora <alexandre.franquet@epitech.eu>
2023-04-16 11:46:33 +0200

    refacto(server/user/tests): refacto tests

--- src/server/tests/test_add_user_with_uuid.c
+++ src/server/tests/test_add_user_with_uuid.c
@@ -9,10 +9,10 @@
 
 #include "data_core.h"
 
-Test(add_user_with_uuid, casual)
-{
+Test(add_user_with_uuid, casual) {
     struct user_head head = init_list_of_users();
-    int ret = add_user_with_uuid(&head, "test", "00000000-0000-0000-0000-000000000000");
+    int ret = add_user_with_uuid(&head, "test",
+    "00000000-0000-0000-0000-000000000000");
     cr_assert_eq(ret, 0);
     int len = 0;
     user_t *user;
