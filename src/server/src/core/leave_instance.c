ee7c64adadb9d8236b6b689e982e835b813c8935
queng <quentin.brejoin@gmail.com>
2023-04-20 22:59:52 +0200

    fix(server): add security when client leave instance

--- src/server/src/core/leave_instance.c
+++ src/server/src/core/leave_instance.c
@@ -7,6 +7,16 @@
 
 #include "server.h"
 
+static void clear_instance(instance_t *instance)
+{
+    close(instance->socket);
+    instance->socket = -1;
+    uuid_clear(instance->user_uuid);
+    uuid_clear(instance->team_uuid);
+    uuid_clear(instance->channel_uuid);
+    uuid_clear(instance->thread_uuid);
+}
+
 void leave_instance(server_t *server, instance_t *instance)
 {
     char buffer[1019];
@@ -14,16 +24,16 @@ void leave_instance(server_t *server, instance_t *instance)
     char uuid[37];
 
     printf("Client disconnected\n");
+    if (uuid_is_null(instance->user_uuid)) {
+        close(instance->socket);
+        instance->socket = -1;
+        return;
+    }
     memset(buffer, 0, 1019);
     uuid_unparse(instance->user_uuid, uuid);
     user = get_user_by_uuid(&server->users, uuid);
     sprintf(buffer, "%s \"%s\"", uuid, user->username);
     server_event_user_logged_out(uuid);
-    close(instance->socket);
-    instance->socket = -1;
-    uuid_clear(instance->user_uuid);
-    uuid_clear(instance->team_uuid);
-    uuid_clear(instance->channel_uuid);
-    uuid_clear(instance->thread_uuid);
+    clear_instance(instance);
     send_message_every_users(server, "SU06", buffer);
 }
