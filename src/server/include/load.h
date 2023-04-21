617a170f0b04837e36a7a726e81f1d20d291e4d8
quentin <Quentin.brejoin@gmail.com>
2023-04-21 14:51:09 +0200

    33 writing reading threads (#69)
    * feat(server): add loading threads
    
    * fix(server): coding style
    
    * fix(client): header
    
    ---------
    
    Co-authored-by: Miouzora <91665686+Miou-zora@users.noreply.github.com>

--- src/server/include/load.h
+++ src/server/include/load.h
@@ -7,8 +7,12 @@
 
 #pragma once
 
+#define _GNU_SOURCE
+
 #include "server.h"
+#include <time.h>
 
 int load_users(server_t *server);
 int load_teams(server_t *server);
 int load_channels(server_t *server);
+int load_threads(server_t *server);
